9f4b2bbe74a0ab3555e57694c435f07a25ad33da
Zodi <fodorzoltan00@gmail.com>
2023-04-20 05:00:07 +0200

    Added return type declaration for getAppletLanguageFile()

--- src/LanguageBatchBo.php
+++ src/LanguageBatchBo.php
@@ -168,7 +168,7 @@ class LanguageBatchBo
 	 *
 	 * @return string|false   The content of the language file or false if weren't able to get it.
 	 */
-	protected static function getAppletLanguageFile($applet, $language)
+	protected static function getAppletLanguageFile($applet, $language): array
 	{
 		$result = ApiCall::call(
 			'system_api',
